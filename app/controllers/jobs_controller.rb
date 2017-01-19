8353c551e74018fff6f984cf4051bfe3ae633bf9
FreshGeek001 <memory2026@126.com>
2017-01-19 17:27:31 +0800

    decoration

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,6 +10,7 @@ class JobsController < ApplicationController
             else
               Job.published.order("created_at DESC")
             end
+    @jobs = @jobs.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
